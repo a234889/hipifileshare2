97ee71ad5914c0df71a6d3ed323eaadcd0d587a2
vipin chauhan <vipin.chauhan4026@gmail.com>
2022-06-24 12:17:58 +0530

    Update DownloadFromAzure.java

--- hipi-azure-function/src/main/java/volvo/com/hipi/helper/DownloadFromAzure.java
+++ hipi-azure-function/src/main/java/volvo/com/hipi/helper/DownloadFromAzure.java
@@ -1,19 +1,5 @@
 package volvo.com.hipi.helper;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.azure.core.http.rest.PagedIterable;
 import com.azure.data.tables.TableClient;
 import com.azure.data.tables.models.TableEntity;
@@ -23,6 +9,14 @@ import com.azure.storage.file.datalake.DataLakeFileClient;
 import com.azure.storage.file.datalake.DataLakeFileSystemClient;
 import com.azure.storage.file.datalake.DataLakeServiceClient;
 import com.azure.storage.file.datalake.models.PathItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 
 public class DownloadFromAzure {
@@ -58,64 +52,6 @@ public class DownloadFromAzure {
 	    return new HashSet<>();
 	}
 
-	
-	/*public List<File> getAttachmentsFromAzureBlob(String reportNo, String reportId, String reportType) throws Exception {
-		logger.info("Downloading files for report : "+reportNo);
-		if(reportId == null ||reportId== null || reportType== null){
-			throw new Exception("reportnumber , reportid and reportType required ");
-		}
-		Set<String> reportIdAttachmentBlobIds =new HashSet<>();// getBlobIdsForReportIdNum(reportNo,reportId,reportType);
-		reportIdAttachmentBlobIds.add("309909");
-		reportIdAttachmentBlobIds.add("311941");
-
-		Date d = new Date(System.currentTimeMillis());
-		
-		String timestamp = d.toString().replaceAll(":", "_");
-		DataLakeServiceClient dataLakeServiceClient = AzureBlobStorageClient.GetDataLakeServiceClient();
-		System.out.println(dataLakeServiceClient + "  "+ dataLakeServiceClient);
-		DataLakeFileSystemClient dataLakeFileSystemClient = dataLakeServiceClient.getFileSystemClient("protusfiles");
-		System.out.println(dataLakeFileSystemClient + "  "+ dataLakeFileSystemClient);
-		List<File> files = new ArrayList<>();
-		DataLakeDirectoryClient directoryClient = dataLakeFileSystemClient.getDirectoryClient(reportType+"Files").getSubdirectoryClient(reportNo);
-		System.out.println("directoryClient   "+ directoryClient.getDirectoryName());
-		PagedIterable<PathItem> pagedIterable = directoryClient.listPaths();
-		java.util.Iterator<PathItem> iterator = pagedIterable.iterator();
-		System.out.println("iterator   "+ iterator);
-		DataLakeFileClient fileClient;
-		File file;
-		String filename;
-		PathItem item;
-		try{
-		while (iterator.hasNext()) {
-			  item = iterator.next();
-			    System.out.println(item.getName());
-				if (item!=null && !item.isDirectory()) {
-                    
-					System.out.println(item.getName());
-					filename = item.getName().substring(item.getName().lastIndexOf("/") + 1);
-					if (matchingBlobId(reportIdAttachmentBlobIds, filename)) {
-						System.out.println(filename);
-						fileClient = directoryClient.getFileClient(filename);
-						file = new File(timestamp + filename);
-						copyInputStreamToFile(fileClient.openInputStream().getInputStream(), file);
-						files.add(file);
-					}
-					
-				} else if(reportId != null && !"".equals(reportId)) {
-					 String  dirName= item.getName().substring(item.getName().lastIndexOf("/") + 1);
-				     System.out.println(dirName );
-				     addReportFilesFromSubDir(reportId, dirName, directoryClient, timestamp,files);
-				}
-		}
-
-		} catch(Exception e){
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-
-		}
-		System.out.println(files);
-		return files;
-	}*/
 
 	public List<File> getAttachmentsFromAzureBlob(String reportNo, String reportId, String reportType) throws Exception {
 		logger.info("Downloading files for report : "+reportNo);
@@ -132,7 +68,7 @@ public class DownloadFromAzure {
 		List<File> files = new ArrayList<>();
 		DataLakeDirectoryClient directoryClient = dataLakeFileSystemClient.getDirectoryClient("Attachments").getSubdirectoryClient(reportType+"Files").getSubdirectoryClient(reportNo);
 		System.out.println(directoryClient.getDirectoryUrl());
-		logger.info(directoryClient.getDirectoryUrl());
+		logger.info("directoryClient: "+directoryClient.getDirectoryUrl());
 		DataLakeFileClient fileClient ;//= directoryClient.getFileClient("pr_mig_1092286_DQ6376M.pdf");
 		File file; //=  new File(timestamp + "pr_mig_1092286_DQ6376M.pdf");
 
@@ -143,17 +79,17 @@ public class DownloadFromAzure {
 		java.util.Iterator<PathItem> iterator = pagedIterable.iterator();
 		String filename;
 		PathItem item;
-		try{
+		//try{
 			while (iterator.hasNext()) {
 				item = iterator.next();
 				if (item!=null && !item.isDirectory()) {
 
 					System.out.println(item.getName());
-					logger.info(item.getName());
+					logger.info("filename "+item.getName());
 					filename = item.getName().substring(item.getName().lastIndexOf("/") + 1);
 					if (matchingBlobId(reportIdAttachmentBlobIds, filename)) {
 						System.out.println(filename);
-						logger.info(item.getName());
+						logger.info("filename "+item.getName());
 						fileClient = directoryClient.getFileClient(filename);
 						file = new File(timestamp + filename);
 
@@ -174,10 +110,10 @@ public class DownloadFromAzure {
 				}
 			}
 
-		} catch(Exception e){
+		/*} catch(Exception e){
 			e.printStackTrace();
 			logger.info("Exception : "+ e.getMessage());
-		}
+		}*/
 		return files;
 	}
 
@@ -191,7 +127,7 @@ public class DownloadFromAzure {
 			File file;
 			PathItem item;
 			DataLakeFileClient fileClient;
-			try{
+			//try{
 			 while (iterator.hasNext()) {
 				  item = iterator.next();
 					if (item!=null && !item.isDirectory()) {
@@ -206,10 +142,10 @@ public class DownloadFromAzure {
 					} 
 			}
 
-			} catch(Exception e){
+			/*} catch(Exception e){
 				e.printStackTrace();
 				logger.info("Exception : "+ e.getMessage());
-			}
+			}*/
 		 }
 	}
 
